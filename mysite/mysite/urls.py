337f698337cfb466c686e86df42b14155fcf9a33
Sheraram Prajapat <sheraramprajapat1998@gmail.com>
2020-02-27 17:41:06 +0530

    sending e-mails with Django. closes #18

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('<int:post_id>/share/',views.post_share, name='post_share'),
 
 ]
